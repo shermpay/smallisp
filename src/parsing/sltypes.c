306976e35e3e8b75aff5aa4b7ebd7898ec9931eb
shermpay <shermanpay1991@gmail.com>
2014-06-13 16:10:50 -0700

    Branch for Smallisp Types. WIP: Object type

---
+++ src/parsing/sltypes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sltypes.h"
+
+Object *new_object(SlType x, int type)
+{
+    Object *res = malloc(sizeof(Object));
+    res->type = type;
+    switch (type) {
+	case SL_NUM:
+	    res->val.sl_num = x.sl_num;
+	    break;
+	case SYMBOL:
+	    res->val.symbol = x.symbol;
+	    break;
+	case SL_CHAR:
+	    res->val.sl_char = x.sl_char;
+	    break;
+	case SL_BOOL:
+	    res->val.sl_bool = x.sl_bool;
+	    break;
+	case LIST:
+	    res->val.list = x.list;
+	    break;
+	default:
+	    fprintf(stderr, "Invalid Type\n");
+	    return 0;
+    }
+    return res;
+}
+
+void del_object(Object *x)
+{
+    
+}
